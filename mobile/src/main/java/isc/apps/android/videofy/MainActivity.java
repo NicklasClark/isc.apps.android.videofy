eca90924555d09fc758e8744cd465720fd37aabb
Suriyaa Kudo <isc.suriyaa@gmail.com>
2015-07-26 14:49:11 +0200

    Edit files with Android Studio

---
+++ mobile/src/main/java/isc/apps/android/videofy/MainActivity.java
@@ -0,0 +1,75 @@
+package isc.apps.android.videofy;
+
+        import android.os.Bundle;
+        import android.app.Activity;
+        import android.app.AlertDialog;
+        import android.app.ProgressDialog;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.Window;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+        import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+    private ProgressDialog progressBar;
+    private static final String TAG = "Main";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.trnity);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        progressBar = new ProgressDialog(MainActivity.this);
+        progressBar.setMessage("Trinity tuts loading...");
+        progressBar.setCancelable(false);
+        progressBar.setCanceledOnTouchOutside(false);
+        progressBar.show();
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.i(TAG, "Loading");
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                Log.i(TAG, "Done loading " + url);
+                if (progressBar.isShowing()) {
+                    progressBar.dismiss();
+                }
+            }
+
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                Log.e(TAG, "Error: " + description);
+                Toast.makeText(getApplicationContext(),
+                        "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage(description);
+                alertDialog.setCancelable(false);
+                alertDialog.show();
+
+            }
+        });
+        webView.loadUrl("https://isc-host.github.io/videos.isc");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
